d7da9239b44fecdd395e235e979a4e07b7228de1
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-01-09 13:14:37 +0900

    ✨ (Add): Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
-import "styles/global.scss";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} />
-  </>
-);
+import * as gtag from "lib/gtag";
+
+import "styles/global.scss";
+
+const MyApp = ({ Component, pageProps, router: routerProp }: AppProps): JSX.Element => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string): void => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  return (
+    <>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} key={routerProp.asPath} />
+    </>
+  );
+};
 
 export default MyApp;
