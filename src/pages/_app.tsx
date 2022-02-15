c71a86ee405b4bc3915e9aad7164ea2255d3ea2b
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-02-15 17:25:29 +0900

    feat: ie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,27 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
+import type { NextPage } from "next";
 
 import * as gtag from "lib/gtag";
 
 import "styles/global.scss";
+
+const ConfirmIfIE: NextPage = () => {
+  const router = useRouter();
+  if (process.browser) {
+    const ua = window.navigator.userAgent.toLowerCase();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      if (ua.indexOf("msie") !== -1 || ua.indexOf("trident") !== -1) {
+        router.replace("/ie");
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+  }
+
+  return null;
+};
 
 const MyApp = ({ Component, pageProps, router: routerProp }: AppProps): JSX.Element => {
   const router = useRouter();
@@ -21,6 +38,7 @@ const MyApp = ({ Component, pageProps, router: routerProp }: AppProps): JSX.Elem
     <>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} key={routerProp.asPath} />
+      <ConfirmIfIE />
     </>
   );
 };
