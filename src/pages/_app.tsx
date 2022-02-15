c5d6c01859f7336f7f7c3e96cec6093528137e88
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-02-15 21:02:19 +0900

    🎨 (Update): reject ie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,20 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
-import type { NextPage } from "next";
 
 import * as gtag from "lib/gtag";
-
 import "styles/global.scss";
-
-const ConfirmIfIE: NextPage = () => {
-  const router = useRouter();
-  if (process.browser) {
-    const ua = window.navigator.userAgent.toLowerCase();
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      if (ua.indexOf("msie") !== -1 || ua.indexOf("trident") !== -1) {
-        router.replace("/ie");
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-  }
-
-  return null;
-};
 
 const MyApp = ({ Component, pageProps, router: routerProp }: AppProps): JSX.Element => {
   const router = useRouter();
   useEffect(() => {
+    if (typeof window !== undefined) {
+      const UA = window.navigator.userAgent.toLowerCase();
+      if (UA.indexOf("msie") !== -1 || UA.indexOf("trident") !== -1) {
+        router.replace("/ie");
+      }
+    }
+
     const handleRouteChange = (url: string): void => {
       gtag.pageview(url);
     };
@@ -33,12 +22,12 @@ const MyApp = ({ Component, pageProps, router: routerProp }: AppProps): JSX.Elem
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
-  }, [router.events]);
+  }, [router]);
+
   return (
     <>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} key={routerProp.asPath} />
-      <ConfirmIfIE />
     </>
   );
 };
--- src/pages/ie.tsx
+++ src/pages/ie.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Layout from "components/Layout";
 
 const Ie: NextPage = () => (
-  <Layout PageTitle="Stop using IE!">
+  <Layout PageTitle="Don't Use IE">
     <div>
       <h1>IEEEEEE</h1>
     </div>
