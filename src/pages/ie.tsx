7dee3c0f16d34f4b76846e8251b218634ecc2c35
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-02-15 17:23:44 +0900

    add: ie.tsx

---
+++ src/pages/ie.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Layout from "components/Layout";
+
+const Ie: NextPage = () => (
+  <Layout PageTitle="Stop using IE!">
+    <div>
+      <h1>IEEEEEE</h1>
+    </div>
+  </Layout>
+);
+
+export default Ie;
