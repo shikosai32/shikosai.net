bffccc762ceb7293b0800ff471c9ae056f90e053
koutyuke <gunji.kousuke08@gmail.com>
2022-06-11 18:41:53 +0900

    🔨 (Update): css animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      transitionTimingFunction:{
-        "burgerMenu":"cubic-bezier(.53,0,1,.67)"
+      fontFamily: {
+        roboto_serif: ["Roboto Serif"],
       },
-      transitionDelay:{
-        "0":"0ms",
-        "25":"25ms",
-        "50":"50ms",
-        "75":"75ms",
-        "100":"100ms",
-        "125":"125ms",
-        "150":"150ms",
-        "125":"125ms",
-        "150":"150ms",
-        "175":"175ms",
-        "200":"200ms"
-      },
-      spacing:{
-        "1/10":"10%"
+      keyframes:{
+        display:{
+          from:{opacity: 0},
+          to:{opacity: 1}
+        },
+        hide:{
+          from:{opacity: 1},
+          to:{opacity: 0}
+        }
       }
     },
   },
