ff51f247f05001f76d4cba5ce34fa652463d42cb
koutyuke <gunji.kousuke08@gmail.com>
2022-06-11 18:50:43 +0900

    🔨 (Update): header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,38 @@ import { FC } from "react";
 import HanbergerMenu from "components/HanbergerMemu";
 
 const Header: FC = () => (
-  <div className="w-screen sticky top-0 flex justify-center">
-    <HanbergerMenu />
+  <div className="w-full h-20 fixed top-0 z-10 bg-white drop-shadow-md font-roboto_serif">
+    <div className="hidden md:block w-full h-full">
+      <div className="h-full w-full flex items-center justify-center space-x-8">
+        <a href="./access" rel="noopener noreferrer" className="font-semibold">
+          ACCESS
+        </a>
+        <a href="./circle" rel="noopener noreferrer" className="font-semibold">
+          CIRCLE
+        </a>
+        <a href="./" rel="noopener noreferrer" className="flex items-center space-x-2">
+          <div className="font-extrabold">The</div>
+          <img src="image/black/logo.png" alt="logo" className="h-12 w-12 border-[3px] border-black" />
+          <div className="font-extrabold">32nd.</div>
+        </a>
+        <a href="./timetable" rel="noopener noreferrer" className="font-semibold">
+          TIMETABLE
+        </a>
+        <a href="./staff" rel="noopener noreferrer" className="font-semibold">
+          STAFF
+        </a>
+      </div>
+    </div>
+    <div className="md:hidden w-full h-full">
+      <div className="w-full h-full p-2 flex justify-between items-center relative">
+        <a href="./" rel="noopener noreferrer" className="flex items-center space-x-1">
+          <div className="font-extrabold">The</div>
+          <img src="image/black/logo.png" alt="logo" className="h-10 w-10 border-[2px] border-black" />
+          <div className="font-extrabold">32nd.</div>
+        </a>
+        <HanbergerMenu />
+      </div>
+    </div>
   </div>
 );
 
