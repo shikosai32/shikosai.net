2f9c7d414b27a10d078cd495fa6fe249b4a91c0e
koutyuke <gunji.kousuke08@gmail.com>
2022-06-11 21:46:09 +0900

    🔨 (Update): add space

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
       fontFamily: {
         roboto_serif: ["Roboto Serif"],
       },
-      keyframes:{
-        display:{
-          from:{opacity: 0},
-          to:{opacity: 1}
+      keyframes: {
+        display: {
+          from:  {opacity: 0},
+          to: {opacity: 1}
         },
-        hide:{
-          from:{opacity: 1},
-          to:{opacity: 0}
+        hide: {
+          from: {opacity: 1},
+          to: {opacity: 0}
         }
       }
     },
