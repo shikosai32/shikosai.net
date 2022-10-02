644c8f4dcfdae17838c9e304ee111cebff8c450b
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-10-02 16:00:30 +0900

    ✨ (Update): countdown component

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,7 +2,7 @@ import moment from "moment";
 import { FC, useState } from "react";
 
 const Time: FC = () => {
-  const shikosaiTime = moment("2022-10-22");
+  const shikosaiTime = moment("2022-10-22T10:00");
   const diff = shikosaiTime.diff(moment());
   const [time, setTime] = useState(diff);
   const dura = moment.duration(time + 1000);
@@ -12,12 +12,9 @@ const Time: FC = () => {
   return (
     <div className="flex flex-col items-center justify-center font-light">
       <span className="hidden text-4xl underline decoration-2 underline-offset-8 md:block">
-        {dura.months()}Months | {dura.days()}Days | {dura.hours()}Hours | {dura.minutes()}Minutes | {dura.seconds()}
-        Seconds
+        {dura.days()}Days | {dura.hours()}Hours | {dura.minutes()}Minutes | {dura.seconds()}Seconds
       </span>
-      <span className="text-xl md:hidden">
-        {dura.months()}Months | {dura.days()}Days
-      </span>
+      <span className="text-xl md:hidden">{dura.days()}Days</span>
       <span className="text-xl md:hidden">
         {dura.hours()}Hours | {dura.minutes()}Minutes | {dura.seconds()}Seconds
       </span>
