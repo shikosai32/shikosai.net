de0a99bb3b991e108ee5a28e0e1f541fe710846b
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-10-02 13:32:36 +0900

    🔧(Fix): replace next/link

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        montserrat:["Montserrat"]
+        montserrat: ["Montserrat"],
       },
       keyframes: {
         display: {
