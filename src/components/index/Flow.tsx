6bf7de9d75e55a96f095bc523e57b7d5825036cb
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-03 08:39:43 +0900

    feat: ✨ (index/Flow) ボタン押下時にページ遷移をする処理を追加

--- src/components/index/Flow.tsx
+++ src/components/index/Flow.tsx
@@ -1,9 +1,44 @@
+import Router from "next/router";
 import { FC, ReactNode } from "react";
 import flowItems from "./flowItems";
 
-const Box: FC<{ className: string; children: ReactNode }> = ({ className, children }) => (
+const onClick = (title: "timetable" | "circle" | "access" | "staff") => {
+  switch (title) {
+    case "timetable":
+      Router.push("/timetable");
+      break;
+    case "circle":
+      Router.push("/circle");
+      break;
+    case "access":
+      Router.push("/access");
+      break;
+    case "staff":
+      Router.push("/staff");
+      break;
+    default:
+      break;
+  }
+};
+
+const FlowBox: FC<{ className: string; children: ReactNode; type: "timetable" | "circle" | "access" | "staff" }> = ({
+  className,
+  children,
+  type,
+}) => (
   <div className="relative">
-    <div className={`h-[146px] rounded-lg ${className}`}>{children}</div>
+    <button
+      type="button"
+      onClick={() => {
+        onClick(type);
+      }}
+    >
+      <div
+        className={`h-[146px] rounded-lg ${className} transition hover:-translate-y-1 hover:-translate-x-1 active:translate-y-3 active:translate-x-3`}
+      >
+        {children}
+      </div>
+    </button>
     <div className="absolute top-3 left-3 -z-10 h-[146px] w-full rounded-lg bg-black" />
   </div>
 );
@@ -12,7 +47,10 @@ const Flow: FC = () => (
   <>
     {flowItems.map((flowItem, index) => (
       <div key={flowItem.id} className="relative">
-        <Box className={`grid grid-cols-2 ${index % 2 === 0 ? "bg-primary" : "bg-secondary"}`}>
+        <FlowBox
+          type={flowItem.title}
+          className={`grid grid-cols-2 ${index % 2 === 0 ? "bg-primary" : "bg-secondary"}`}
+        >
           <div className="flex items-center justify-center before:absolute before:left-1/2 before:h-3/4 before:w-[2px] before:bg-black before:content-['']">
             <img src={`/image/white/${flowItem.title}.png`} alt={flowItem.title} width="50%" height="50%" />
           </div>
@@ -20,7 +58,7 @@ const Flow: FC = () => (
             <span className="text-3xl">{flowItem.subtitle}</span>
             <span className="text-2xl">{flowItem.description}</span>
           </div>
-        </Box>
+        </FlowBox>
         <div>
           <img
             src="image/arrow-to-bottom.svg"
