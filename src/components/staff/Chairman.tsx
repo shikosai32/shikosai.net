243cc921203fd7d845665e2039529f2182fa2f51
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-03 13:29:16 +0900

    feat: ✨ Chairmanコンポーネントを作成

---
+++ src/components/staff/Chairman.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import Card from "../Card";
+
+const Chairman: FC = () => (
+  <Card className="rounded-xl py-10">
+    <p className="relative text-xl before:absolute before:top-16 before:h-[2px] before:w-full before:bg-gray-400 before:content-['']">
+      実行委員長
+      <br />
+      hoge
+    </p>
+    <p className="mt-12">こんにちは、第32回茨香祭実行委員長のほげほげです。 今年の茨香祭のテーマは「Pop Up」です。</p>
+  </Card>
+);
+
+export default Chairman;
