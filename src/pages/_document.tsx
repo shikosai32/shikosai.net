c4e8cdbe0164cc84c89b38ba5bbae6d3ef1f577f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-03 13:27:48 +0900

    wip: 🚧 Noto Serif JPをフォントファミリーに追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,6 +25,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
