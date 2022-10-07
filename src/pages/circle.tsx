0d783fbc650edd020d92727aa3bd6f2ea2177b85
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-10-07 17:28:11 +0900

    fix: circle

--- src/pages/circle.tsx
+++ src/pages/circle.tsx
@@ -4,7 +4,7 @@ import Layout from "~/components/Layout";
 const Circle: NextPage = () => (
   <Layout PageTitle="Circle | 第32回茨香祭HP">
     <figure className="my-10 flex justify-center">
-      <img src="image/black/circle.png" alt="staff" width="200" />
+      <img src="image/black/circle.png" alt="circle" width="200" />
     </figure>
     <p className="text-center text-3xl font-light italic">Coming Soon</p>
   </Layout>
