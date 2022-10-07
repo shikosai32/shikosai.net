c36ba436d937aff93407462af4d3c30730e61795
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-10-07 17:25:19 +0900

    feat: Coming Soon

---
+++ src/pages/circle.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Layout from "~/components/Layout";
+
+const Circle: NextPage = () => (
+  <Layout PageTitle="Staff | 第32回茨香祭HP">
+    <figure className="my-10 flex justify-center">
+      <img src="image/black/circle.png" alt="staff" width="200" />
+    </figure>
+    <p className="text-center text-3xl font-light italic">Coming Soon</p>
+  </Layout>
+);
+
+export default Circle;
---
+++ src/pages/staff.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Layout from "~/components/Layout";
+
+const Staff: NextPage = () => (
+  <Layout PageTitle="Staff | 第32回茨香祭HP">
+    <figure className="my-10 flex justify-center">
+      <img src="image/black/staff.png" alt="staff" width="200" />
+    </figure>
+    <p className="text-center text-3xl font-light italic">Coming Soon</p>
+  </Layout>
+);
+
+export default Staff;
