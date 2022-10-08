def6c29ed41c51ab23e3afbb282bcbf953471997
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-10-09 01:36:59 +0900

    🔧(fix): fix import path

--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import Layout from "~/components/Layout";
+import Layout from "~/components/Layout/Layout";
 
 const Staff: NextPage = () => (
   <Layout PageTitle="Staff">
