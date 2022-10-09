a2af20532d00ae1557274dda603b39fde47b3bf8
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-10 01:27:08 +0900

    fix: 🐞 (staff/Chairman) フォントを修正

--- src/components/staff/Chairman.tsx
+++ src/components/staff/Chairman.tsx
@@ -3,12 +3,14 @@ import Card from "../Card";
 
 const Chairman: FC = () => (
   <Card className="rounded-xl py-10">
-    <p className="relative text-xl before:absolute before:top-16 before:h-[2px] before:w-full before:bg-gray-400 before:content-['']">
+    <p className="relative font-noto-serif-jp text-xl before:absolute before:top-16 before:h-[2px] before:w-full before:bg-gray-400 before:content-['']">
       実行委員長
       <br />
-      hoge
+      佐藤鉄平
     </p>
-    <p className="mt-12">こんにちは、第32回茨香祭実行委員長のほげほげです。 今年の茨香祭のテーマは「Pop Up」です。</p>
+    <p className="mt-12 font-noto-serif-jp">
+      こんにちは、第32回茨香祭実行委員長のほげほげです。 今年の茨香祭のテーマは「Pop Up」です。
+    </p>
   </Card>
 );
 
