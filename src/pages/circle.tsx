1fc5dd8dea3968db9cd9835a4fd4a1efcd87b8d0
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-10-12 21:28:31 +0900

    🎨 (Update): circle page

--- src/pages/circle.tsx
+++ src/pages/circle.tsx
@@ -6,25 +6,25 @@ import circleData from "~/static/circleData";
 
 const Circle: NextPage = () => (
   <Layout PageTitle="Circle - shikosai32">
-    <div className="font-roboto_serif relative flex h-full w-full flex-col items-center justify-around">
+    <div className="font-roboto_serif relative flex h-full w-full flex-col items-center justify-around pt-10">
+      <img src="/image/black/circle.png" className="mx-auto mb-4 h-10 md:h-12 lg:h-20" alt="Circle" />
       <div className="w-[calc(100%_-_5px)] md:w-full">
-        <div className="text-center">
-          <p className="text-center">JUMP TO</p>
-          <ul className="flex flex-wrap">
-            {circleData.classify.map((v) => (
-              <li key={v} className="w-1/2 md:w-1/6">
-                <Link href={`#${v}`}>{v}</Link>
-              </li>
+        <div className="my-6 text-center">
+          <h2>JUMP TO</h2>
+          <div className="flex flex-wrap">
+            {circleData.classify.map((v, i) => (
+              <button type="button" key={v} className="w-1/2 md:w-1/6">
+                <Link href={`#${circleData.ids[i]}`}>{v}</Link>
+              </button>
             ))}
-          </ul>
-        </div>
-        <div className="w-full content-center" id="1学年">
-          <div className="w-full rounded border-2 border-black lg:w-full">
-            <p className="text-center text-2xl">1学年</p>
           </div>
-          <div className="flex w-full flex-wrap pb-[5%]">
-            {circleData.content.firstGrade.map((value) =>
-              value.map((v) => (
+        </div>
+
+        {circleData.classify.map((value, i) => (
+          <div key={value} className="-mt-24 w-full content-center pt-24" id={circleData.ids[i]}>
+            <h3 className="rounded border-2 border-black text-center text-2xl">{value}</h3>
+            <div className="flex w-full flex-wrap pb-[5%]">
+              {circleData.contents[i].map((v) => (
                 <CardModal
                   key={v.organization}
                   org={v.organization}
@@ -35,115 +35,10 @@ const Circle: NextPage = () => (
                   about={v.aboutCircle}
                   map={v.mapImagePth}
                 />
-              )),
-            )}
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="w-full content-center" id="2学年">
-          <div className="w-full rounded border-2 border-black lg:w-full">
-            <p className="text-center text-2xl">2学年</p>
-          </div>
-          <div className="flex w-full flex-wrap pb-[5%]">
-            {circleData.content.secondGrade.map((value) =>
-              value.map((v) => (
-                <CardModal
-                  key={v.organization}
-                  org={v.organization}
-                  title={v.circleTitle}
-                  placement={v.placement}
-                  imgPath={v.circleImagePth}
-                  description={v.description}
-                  about={v.aboutCircle}
-                  map={v.mapImagePth}
-                />
-              )),
-            )}
-          </div>
-        </div>
-        <div className="w-full content-center" id="3学年">
-          <div className="w-full rounded border-2 border-black lg:w-full">
-            <p className="text-center text-2xl">3学年</p>
-          </div>
-          <div className="flex w-full flex-wrap pb-[5%]">
-            {circleData.content.thirdGrade.map((value) =>
-              value.map((v) => (
-                <CardModal
-                  key={v.organization}
-                  org={v.organization}
-                  title={v.circleTitle}
-                  placement={v.placement}
-                  imgPath={v.circleImagePth}
-                  description={v.description}
-                  about={v.aboutCircle}
-                  map={v.mapImagePth}
-                />
-              )),
-            )}
-          </div>
-        </div>
-        <div className="w-full content-center" id="4学年">
-          <div className="w-full rounded border-2 border-black lg:w-full">
-            <p className="text-center text-2xl">4学年</p>
-          </div>
-          <div className="flex w-full flex-wrap pb-[5%]">
-            {circleData.content.fourthGrade.map((value) =>
-              value.map((v) => (
-                <CardModal
-                  key={v.organization}
-                  org={v.organization}
-                  title={v.circleTitle}
-                  placement={v.placement}
-                  imgPath={v.circleImagePth}
-                  description={v.description}
-                  about={v.aboutCircle}
-                  map={v.mapImagePth}
-                />
-              )),
-            )}
-          </div>
-        </div>
-        <div className="w-full content-center" id="部活動">
-          <div className="w-full rounded border-2 border-black lg:w-full">
-            <p className="text-center text-2xl">部活動</p>
-          </div>
-          <div className="flex w-full flex-wrap pb-[5%]">
-            {circleData.content.act.map((value) =>
-              value.map((v) => (
-                <CardModal
-                  key={v.organization}
-                  org={v.organization}
-                  title={v.circleTitle}
-                  placement={v.placement}
-                  imgPath={v.circleImagePth}
-                  description={v.description}
-                  about={v.aboutCircle}
-                  map={v.mapImagePth}
-                />
-              )),
-            )}
-          </div>
-        </div>
-        <div className="w-full content-center" id="同好会">
-          <div className="w-full rounded border-2 border-black lg:w-full">
-            <p className="text-center text-2xl">同好会</p>
-          </div>
-          <div className="flex w-full flex-wrap pb-[5%]">
-            {circleData.content.club.map((value) =>
-              value.map((v) => (
-                <CardModal
-                  key={v.organization}
-                  org={v.organization}
-                  title={v.circleTitle}
-                  placement={v.placement}
-                  imgPath={v.circleImagePth}
-                  description={v.description}
-                  about={v.aboutCircle}
-                  map={v.mapImagePth}
-                />
-              )),
-            )}
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   </Layout>
