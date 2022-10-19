300837c13da00ea63371bb1101d6c289b3f24c5b
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-10-20 01:04:25 +0900

    ✨ (Add): hp link component

---
+++ src/components/Layout/footer/Site.tsx
@@ -0,0 +1,17 @@
+import type { FC } from "react";
+import Link from "next/link";
+import { MdOutlineLanguage } from "react-icons/md";
+
+const Site: FC = () => (
+  <>
+    <p className="flex items-center	justify-center pt-4 text-2xl underline underline-offset-1">External Site</p>
+    <Link href="https://www.ibaraki-ct.ac.jp/">
+      <a className="flex items-center	justify-center py-2 text-lg" target="_blank" rel="noopener noreferrer">
+        <MdOutlineLanguage className="mr-2" />
+        NITIC HP
+      </a>
+    </Link>
+  </>
+);
+
+export default Site;
