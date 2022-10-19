6310335f5526ed485076d50bb84379e1547b7aa4
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-10-20 01:05:16 +0900

    ✨ (Add): copyright component

---
+++ src/components/Layout/footer/Privacy.tsx
@@ -0,0 +1,14 @@
+import type { FC } from "react";
+import Link from "next/link";
+
+const Privacy: FC = () => (
+  <div className="mt-5 flex w-full flex-col justify-center lg:flex-row">
+    <Link href="/privacy">
+      <a className="my-2 text-center lg:my-0">Privacy Policy</a>
+    </Link>
+    <span className="mx-2 hidden lg:block">|</span>
+    <p className="text-center">&copy;2022 National Institute of Technology (KOSEN), Ibaraki College</p>
+  </div>
+);
+
+export default Privacy;
