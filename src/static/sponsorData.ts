c5806555545cfc173dafb6da319cca827ce689ae
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-10-20 02:31:23 +0900

    🍱 (Add): sponsors data

---
+++ src/static/sponsorData.ts
@@ -0,0 +1,71 @@
+type Data = {
+  name: string;
+  link?: string;
+}[];
+
+const sponsorData: Data = [
+  {
+    name: "ミリオンスポーツ",
+  },
+  {
+    name: "唐揚げ専門店 スターティングオーバー",
+    link: "https://startingover2016.jimdofree.com/",
+  },
+  {
+    name: "イガラシ綜業株式会社",
+    link: "https://www.igarashisogyo.com/",
+  },
+  {
+    name: "海野ガーデン",
+    link: "https://www.uminogarden.com/",
+  },
+  {
+    name: "株式会社Xtraveler",
+    link: "https://about.xtraveler.jp/",
+  },
+  {
+    name: "飯村ストアー",
+  },
+  {
+    name: "ローソンひたちなか北店",
+    link: "https://www.e-map.ne.jp/p/lawson/dtl/232265/",
+  },
+  {
+    name: "すし蔵",
+  },
+  {
+    name: "関東タクシー",
+  },
+  {
+    name: "学秀館",
+  },
+  {
+    name: "有限会社菊池寝具店",
+    link: "https://kikuchi-shingu.com/",
+  },
+  {
+    name: "パン工房ぐるぐる ひたちなか店",
+    link: "https://www.e-guruguru.net/",
+  },
+  {
+    name: "樹里",
+    link: "https://www.chiffon-juri.com/",
+  },
+  {
+    name: "Smile洋菓子店",
+  },
+  {
+    name: "ひたちなか自動車学校",
+    link: "https://hitachinaka-ds.co.jp/",
+  },
+  {
+    name: "ホームシックスタイルショップ",
+    link: "https://www.homesic.com/",
+  },
+  {
+    name: "勝田ラジコン模型",
+    link: "http://katsutaragicon.com/",
+  },
+];
+
+export default sponsorData;
