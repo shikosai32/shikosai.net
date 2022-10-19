ebeb1f780368e7d6a450b3381bc9c4fd082137ef
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-10-20 01:04:56 +0900

    ✨ (Add): sponsors component

---
+++ src/components/Layout/footer/Sponsor.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+import Link from "next/link";
+
+const Sponsor: FC = () => (
+  <div>
+    <p className="text-center text-2xl underline underline-offset-1">Sponsors</p>
+  </div>
+);
+
+export default Sponsor;
