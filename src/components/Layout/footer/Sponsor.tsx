3400a25384b36fc9faf83035b41a3da280cd98fc
yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-10-20 02:31:55 +0900

    ✨ (Add): sponsor component

--- src/components/Layout/footer/Sponsor.tsx
+++ src/components/Layout/footer/Sponsor.tsx
@@ -1,9 +1,23 @@
 import type { FC } from "react";
 import Link from "next/link";
+import sponsorData from "~/static/sponsorData";
 
 const Sponsor: FC = () => (
   <div>
     <p className="text-center text-2xl underline underline-offset-1">Sponsors</p>
+    <div className="flex flex-col items-center justify-center">
+      {sponsorData.map((v) =>
+        v.link ? (
+          <p>{v.name}</p>
+        ) : (
+          <Link href={v.link ?? ""}>
+            <a className="py-1" target="_blank" rel="noopener noreferrer">
+              {v.name}
+            </a>
+          </Link>
+        ),
+      )}
+    </div>
   </div>
 );
 
