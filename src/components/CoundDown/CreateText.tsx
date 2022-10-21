2b02b61019d0c3852ac5f1e3cf9ccdd463d6d142
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-10-22 06:32:09 +0900

    fix: change text

--- src/components/CoundDown/CreateText.tsx
+++ src/components/CoundDown/CreateText.tsx
@@ -4,13 +4,13 @@ import { CreateTextProps } from "./type/model";
 const CreateText: FC<CreateTextProps> = ({ status }) => {
   switch (status) {
     case "before":
-      return <span>Until shikosai is held...</span>;
+      return <span>Until Shikosai Is Held...</span>;
 
     case "first":
       return (
         <>
           <span>1st Day In Progress!!</span>
-          <span>Until the end of the 1st day...</span>
+          <span>Until The End Of The 1st Day...</span>
         </>
       );
 
@@ -18,7 +18,7 @@ const CreateText: FC<CreateTextProps> = ({ status }) => {
       return (
         <>
           <span>Day 1 Has Ended.</span>
-          <span>Until the 2nd day is held...</span>
+          <span>Until The 2nd Day Is Held...</span>
         </>
       );
 
@@ -26,7 +26,7 @@ const CreateText: FC<CreateTextProps> = ({ status }) => {
       return (
         <>
           <span>2nd Day In Session!!</span>
-          <span>Until the end of the 2nd day...</span>
+          <span>Until The End Of The 2nd Day...</span>
         </>
       );
 
